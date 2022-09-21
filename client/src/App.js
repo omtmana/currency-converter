be17648c8d6d4a60147d9b5135b8261cc91c6bb5
Olivia <oliviatmanalastas@gmail.com>
2022-09-21 13:01:06 -0400

    Added links to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
 // import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import './styles/App.css'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Convert from './pages/Convert';
-import Header from './components/Header'
-import Layout from './components/Layout';
+// import Header from './components/Header'
+// import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Layout>
+      <nav>
+        <div>
+          <Link to='/' > HOME </Link>
+          <Link to='/login'> LOGIN </Link>
+          <Link to='/signup'> SIGNUP </Link>
+          <Link to='/convert'> CONVERT </Link>
+        </div>
+      </nav>
+        {/* <Layout> */}
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/convert' element={<Convert />} />
           </Routes>
-        </Layout>
+        {/* </Layout> */}
       </BrowserRouter>
     </div>
   );
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,11 +1,11 @@
-import Header from "./Header"
+import Header from "./Header";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
    return (
-      <>
-      <Header />
-      <main> {children} </main>
-      </>
+      <div>
+         <Header />
+         <main> {children} </main>
+      </div>
    )
 }
 
